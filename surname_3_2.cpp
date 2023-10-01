895c061a0c849570a61cad666d48cf83b3f47d6b
Krazalm <mikhailagero@gmail.com>
2023-10-01 23:36:05 +0800

    Add files via upload

---
+++ surname_3_2.cpp
@@ -0,0 +1,16 @@
+#include iostream
+using namespace std;
+
+int main() {
+    int Number = 10;
+    char Letter = 'a';
+    bool Result = true;
+    float Float = 3.14;
+
+    cout  Number =   Number  endl;
+    cout  Letter =   Letter  endl;
+    cout  Result =   Result  endl;
+    cout  Float =   Float  endl;
+
+    return 0;
+}
